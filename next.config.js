f641ad324f477dea16af2346ccb1c2865028d0a2
Dominik Stumpf <s.dominik.work@gmail.com>
2023-09-14 19:57:55 +0200

    added pwa back

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  // webpack: (config, {isServer}) => {
-  //   if (!isServer) {
-  //     config.externals.push('sharp')
-  //   }
-  //   config.module.rules.push({
-  //     test: /\.(glsl|vs|fs|vert|frag)$/,
-  //     use: 'raw-loader',
-  //   });
-  //   return config;
-  // },
+  reactStrictMode: true,
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.externals.push('sharp')
+    }
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 }
 
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === 'development',
-// });
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+});
 
-// module.exports = withPWA(
-//   nextConfig
-// );
-
-module.exports = nextConfig
+module.exports = withPWA(
+  nextConfig
+);
